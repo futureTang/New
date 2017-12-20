84411c67339d1812e9f1bf49197f93aade08b170
TangYao <1209985002@qq.com>
2017-12-20 20:13:58 +0800

    the first day

---
+++ src/Demo/Print.java
@@ -0,0 +1,15 @@
+package Demo;
+
+public class Print {
+	public static void print(Object object) {
+		System.out.println(object);
+	}
+	
+	public static void print() {
+		System.out.println();
+	}
+	
+	public static void printb(Object object) {
+		System.out.print(object);
+	}
+}
